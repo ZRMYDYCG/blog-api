7c9d455bc240be80cbd966456475e7d2ced19060
ZRMYDYCG <547471919@qq.com>
2025-01-21 11:05:47 +0800

    feat: 区分配置文件

--- packages/serve-nest/src/app.module.ts
+++ packages/serve-nest/src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserModule } from './user/user.module'
+import { User } from './user/entities/user.entity'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import * as path from 'path'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        isProd
+          ? path.resolve('.env.prod')
+          : path.resolve('.env.development.local'),
+      ],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          connectorPackage: 'mysql2', // 驱动包
+          port: configService.get('DB_PORT'), // 端口号
+          username: configService.get('DB_USERNAME'), // 用户名
+          password: configService.get('DB_PASSWORD'), // 密码
+          database: configService.get('DB_DATABASE'), // 数据库名
+          entities: [User], // 数据库对应的Entity
+          autoLoadEntities: true, // 自动加载实体
+          synchronize: !isProd, // 是否自动同步实体文件,生产环境下关闭
+        }
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
